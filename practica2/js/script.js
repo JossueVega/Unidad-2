fc8fdba32ca38aae88cf2f689f91c2513bc6f896
JossueVega <jossue.vega99@outlook.com>
2020-10-28 14:01:26 -0700

    Actualizacion

--- practica2/js/script.js
+++ practica2/js/script.js
@@ -63,7 +63,29 @@ function init(){
         clean();
         console.log(op1);
     }
+    resta.onclick=function(e){
+        op1=resultado.textContent;
+        op='-';
+        clean();
+        console.log(op1);
+    }
 
+    multi.onclick=function(e){
+        op1=resultado.textContent;
+        op='*';
+        clean();
+        console.log(op1);
+    }
+    divi.onclick=function(e){
+        op1=resultado.textContent;
+        op='/';
+        clean();
+        console.log(op1);
+    }
+    igual.onclick=function(e){
+        op2=resultado.textContent;
+        solve();
+    }
 }
 
 function reseting(){
@@ -74,4 +96,30 @@ function reseting(){
 }
 function clean(){
     resultado.textContent="";
+}
+// Aqui se hacen las operaciones 
+function solve(){
+    var result=0;
+
+    switch (op) {
+        case '-':
+           result=parseFloat(op1)-parseFloat(op2); 
+            break;
+
+        case '+':
+            result=parseFloat(op1)+parseFloat(op2); 
+            break;
+        case '*':
+            result=parseFloat(op1)*parseFloat(op2); 
+            break;
+        case '/':
+            result=parseFloat(op1)/parseFloat(op2); 
+            break;
+
+        default:
+            break;
+    }
+
+    clean();
+    resultado.textContent=result;
 }
